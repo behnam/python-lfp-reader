fffe5c5d984084a3401d3c33f502479e4fe78bad
Behnam Esfahbod <behnam@esfahbod.info>
2012-08-11 15:00:17 -0400

    Add missing licenses and copyright information

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python
+#
+# lfp-reader
+# LFP (Light Field Photography) File Reader.
+#
+# http://behnam.github.com/python-lfp-reader/
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2012  Behnam Esfahbod
+
 
 from distutils.core import setup
 
-setup(name="lfp-reader", version="1.1.1",
+
+setup(name="lfp-reader", version="1.1.2",
         description="LFP (Light Field Photography) File Reader",
         long_description=open('README.rst').read(),
 
